003ea12bebb5f4de3d459a1a1d5cc0f93f450b09
Albert <dony.nyc@gmail.com>
2022-06-23 23:38:24 +0400

    New practice

---
+++ JS/Rankings+Genres.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const numberOfFilms = parseInt(prompt('Сколько фильмов Вы уже посмотрели?'));
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    makeRank: function () {
+        for (let i = 0; i < 2; i++) {
+            let
+                name = prompt('Один из последних просмотренных фильмов?'),
+                rank = parseInt(prompt(
+                    `На сколько вы его оцените по десятибальной шкале?`));
+            if (name !== null && rank !== null && name.length < 50) {
+                personalMovieDB.movies[name] = rank;
+            } else {
+                console.log('Error');
+                i--;
+            }
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i < 4; i++) {
+            let genre =
+                prompt(`Назовите ваш любимый жанр под номером ${i}`);
+            personalMovieDB.genres[i - 1] = genre;
+        }
+    },
+    showMyDB: function () {
+        if (personalMovieDB.privat === false) {
+            console.log(personalMovieDB);
+        }
+    }
+};
+
+if (personalMovieDB.count < 10) {
+    console.log('просмотрено довольно мало фильмов');
+} else if (numberOfFilms > 10 && numberOfFilms < 50) {
+    console.log('Вы классический зритель!');
+} else if (numberOfFilms > 50) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Произошла ошибка, вероятно, было введено некорректное число');
+}
+
+personalMovieDB.makeRank();
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyDB();
--- JS/script.js
+++ JS/script.js
@@ -1,51 +1,39 @@
 "use strict";
 
-const numberOfFilms = parseInt(prompt('Сколько фильмов Вы уже посмотрели?'));
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: {},
-    privat: false,
-    makeRank: function () {
-        for (let i = 0; i < 2; i++) {
-            let
-                name = prompt('Один из последних просмотренных фильмов?'),
-                rank = parseInt(prompt(
-                    `На сколько вы его оцените по десятибальной шкале?`));
-            if (name !== null && rank !== null && name.length < 50) {
-                personalMovieDB.movies[name] = rank;
-            } else {
-                console.log('Error');
-                i--;
-            }
-        }
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
     },
-    writeYourGenres: function () {
-        for (let i = 1; i < 4; i++) {
-            let genre =
-                prompt(`Назовите ваш любимый жанр под номером ${i}`);
-            personalMovieDB.genres[i] = genre;
-        }
-    },
-    showMyDB: function () {
-        if (personalMovieDB.privat === false) {
-            console.log(personalMovieDB);
-        }
+    makeTest: function () {
+        console.log(Object.keys(options).length);
     }
 };
 
-if (personalMovieDB.count < 10) {
-    console.log('просмотрено довольно мало фильмов');
-} else if (numberOfFilms > 10 && numberOfFilms < 50) {
-    console.log('Вы классический зритель!');
-} else if (numberOfFilms > 50) {
-    console.log('Вы киноман!');
-} else {
-    console.log('Произошла ошибка, вероятно, было введено некорректное число');
-}
+options.makeTest();
 
-personalMovieDB.makeRank();
-personalMovieDB.writeYourGenres();
-personalMovieDB.showMyDB();
+const { border } = options.colors;
+console.log(border);
+
+
+//удаляет по ключу все свойство
+delete options.makeTest;
+console.log(options);
+
+let counter = 0;  // для подсчета элементов в объекте
+for (let key in options) {
+    counter++;
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+}
+console.log(counter);
+
+console.log(Object.keys(options).length);
