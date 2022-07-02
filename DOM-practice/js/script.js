6470d23a01f8e5a58c37453908504ac15f1e793a
Albert <dony.nyc@gmail.com>
2022-07-02 11:09:20 +0400

    DOM

--- DOM-practice/js/script.js
+++ DOM-practice/js/script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+//эти способы выдают массивоподобный объект HTMLCollection:
+const box = document.getElementById('box');
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+console.log(btns[1]); //обратиться к отдельному элементу через индекс
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+
+
+//эти способы выдают массивоподобный объект NodeList:
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+
+//у него появляется метод forEach 
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart); //вернет первый элемент с таким классом на странице
+
