06f00596653341769c7c1e64e0e011842d267902
Albert <dony.nyc@gmail.com>
2022-10-19 23:51:30 +0400

    promise+

--- Server-Practice/Promise.js
+++ Server-Practice/Promise.js
@@ -1,6 +1,23 @@
 'use strict';
 
-//условный пример callback-hell:
+//пример колбэк функции:
+function func1(callback) {
+    setTimeout(() => {
+        console.log('first goes this!');
+        callback();
+    }, 1500);
+}
+function func2() {
+    setTimeout(() => {
+        console.log('now this!');
+    }, 500);
+}
+
+func1(func2);
+//функция 1 выполняется сначала, лишь затем функция 2 (колбэк)
+
+
+//условный пример callback-hell (много всего асинхронного в одном):
 console.log('запрос данных...');
 
 setTimeout(() => {
@@ -24,7 +41,7 @@ setTimeout(() => {
 //как работает promise:
 console.log('запрос данных...');
 
-const req = new Promise(function (resolve, reject) {
+const req = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log('подготовка данных...');
 
@@ -32,18 +49,55 @@ const req = new Promise(function (resolve, reject) {
             name: 'TV',
             price: 2000
         };
-        //в резолв надо передать какие-то данные (data):
+        //в resolve() надо передать какие-то данные (data):
         resolve(product);
 
     }, 2000);
 });
 
-//метод промисов для обработки ПОЛОЖИТЕЛЬНОГО(!!) результата:
+
+//метод .then для обработки ПОЛОЖИТЕЛЬНОГО(!!) результата:
+
+// req.then((product) => {
+//     const req2 = new Promise((resolve, reject) => {
+//         console.log('данные получены');
+//         setTimeout(() => {
+//             product.status = 'order';
+//             resolve(product);
+//         }, 2000);
+//     });
+// //второй промис используется как коллбэк (можно избежать):
+//     req2.then(data => {
+//         console.log(data);
+//     });
+// });
+
+// тот же код, только в более удобоваримом формате:
 req.then((product) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            product.status = 'order';
+            // resolve(product);
+            //чтоб спровоцировать ошибку:
+            reject();
+        }, 2000);
+    });
+}).then(data => {
     console.log('данные получены');
-    setTimeout(() => {
-        product.status = 'order';
-        console.log(product);
-    }, 2000);
-});
+    console.log(data);
+    data.modify = true;
+    return data;
+    //можно задать последовательность выполнения функций:
+}).then(data => {
+    console.log(data);
+    //сработает при возникновении ошибки и пропустит все then:
+}).catch(() => {
+    console.error('Произошла ошибка!');
+    //метод .finally срабатывает в конце вне зависимости от ошибок и т.п:
+}).finally(() =>
+    console.log('finally'));
+///////////////////
+
+
+//еще о методах promise
 
